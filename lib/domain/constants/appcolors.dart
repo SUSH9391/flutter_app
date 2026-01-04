e251eecade325ced3e513ee8f9c16ac506481f00
SUSH9391 <masrbaliga@gmail.com>
2026-01-04 14:09:30 +0530

    added assets folder and repository->uihelper

--- lib/domain/constants/appcolors.dart
+++ lib/domain/constants/appcolors.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 class AppColors{
     static const Color scaffoldbackground = Color(0xFFF7CB45
-)
+);
 }
---
+++ lib/repository/widgets/uihelper.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+class UiHelper{
+    static CustomImage({required String img}){
+        return Image.asset();
+    }
+}
